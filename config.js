c6e9a56ad9999294114f210e36e534e68de7c7a2
Frost771 <kfrydek@gmail.com>
2026-07-10 14:15:45 +0200

    Načíst nový typ zápasu

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 import "./form-fix.js";
 import "./quick-entry.js";
 import "./goalkeeper-stats-v2.js";
+import "./match-types.js";
 
 export const SUPABASE_URL = "https://hekqktfhtvkzxjyslnde.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_6KGafi-gkmj3KCkdBLM97Q_g1-WZjg3";
