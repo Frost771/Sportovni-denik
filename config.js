f915f7ee5e2011424f4d5898e28dfb86ea26c006
Frost771 <kfrydek@gmail.com>
2026-07-11 13:51:27 +0200

    Add import for surface.js in config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ import "./form-fix.js";
 import "./quick-entry.js";
 import "./goalkeeper-stats-v2.js";
 import "./match-types.js";
+import "./surface.js";
 
 export const SUPABASE_URL = "https://hekqktfhtvkzxjyslnde.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_6KGafi-gkmj3KCkdBLM97Q_g1-WZjg3";
