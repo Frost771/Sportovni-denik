7c758f49ff55346bc64e22921846a3255b9ec0a8
Frost771 <kfrydek@gmail.com>
2026-07-11 14:34:49 +0200

    Add import for season-review-flicker-fix.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ import "./match-types.js";
 import "./surface.js";
 import "./surface-other.js";
 import "./season-review.js";
+import "./season-review-flicker-fix.js";
 
 export const SUPABASE_URL = "https://hekqktfhtvkzxjyslnde.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_6KGafi-gkmj3KCkdBLM97Q_g1-WZjg3";
