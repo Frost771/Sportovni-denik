743ad75abb565918e8129d6398cc41947fd98b46
Frost771 <kfrydek@gmail.com>
2026-07-11 14:42:33 +0200

    Add import for surface-flicker-fix.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ import "./surface.js";
 import "./surface-other.js";
 import "./season-review.js";
 import "./season-review-flicker-fix.js";
+import "./surface-flicker-fix.js";
 
 export const SUPABASE_URL = "https://hekqktfhtvkzxjyslnde.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_6KGafi-gkmj3KCkdBLM97Q_g1-WZjg3";
