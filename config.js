693c7542a0cea3568423eb4e4a41712e42aa6b62
Frost771 <kfrydek@gmail.com>
2026-07-11 15:29:55 +0200

    Add import for records-search.js

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ import "./season-review.js";
 import "./season-review-flicker-fix.js";
 import "./surface-flicker-fix.js";
 import "./structured-notes.js";
+import "./records-search.js";
 
 export const SUPABASE_URL = "https://hekqktfhtvkzxjyslnde.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_6KGafi-gkmj3KCkdBLM97Q_g1-WZjg3";
