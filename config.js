0f85725945a1703b9c278520e8ed2948c2255088
Frost771 <kfrydek@gmail.com>
2026-07-11 14:17:47 +0200

    Add import for season review module

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ import "./goalkeeper-stats-v2.js";
 import "./match-types.js";
 import "./surface.js";
 import "./surface-other.js";
+import "./season-review.js";
 
 export const SUPABASE_URL = "https://hekqktfhtvkzxjyslnde.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_6KGafi-gkmj3KCkdBLM97Q_g1-WZjg3";
