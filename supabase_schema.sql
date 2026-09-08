4f3150a324280688ea7c606761ba7d5f7869d203
Frost771 <kfrydek@gmail.com>
2026-09-08 08:48:34 +0200

    Add kit usage charts and floorball jersey colors: supabase_schema.sql

--- supabase_schema.sql
+++ supabase_schema.sql
@@ -130,7 +130,7 @@ alter table public.sport_entries
   add column if not exists socks_color text;
 alter table public.sport_entries drop constraint if exists sport_entries_kit_colors;
 alter table public.sport_entries add constraint sport_entries_kit_colors check (
-  (jersey_color is null or jersey_color in ('black','yellow','red','blue')) and
+  (jersey_color is null or jersey_color in ('white','black','yellow','red','blue')) and
   (shorts_color is null or shorts_color in ('black','yellow','red')) and
   (socks_color is null or socks_color in ('black','yellow','red','blue'))
 );
