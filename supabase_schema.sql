a7771d9b2e7c9dce751008ac4534b5b7409e98a6
Frost771 <kfrydek@gmail.com>
2026-09-09 10:05:58 +0200

    Add opponent difficulty and kit statistics context: supabase_schema.sql

--- supabase_schema.sql
+++ supabase_schema.sql
@@ -143,3 +143,10 @@ alter table public.sport_entries add constraint sport_entries_kit_scope check (
   ))
 );
 commit;
+
+begin;
+alter table public.sport_entries add column if not exists opponent_difficulty integer;
+alter table public.sport_entries drop constraint if exists sport_entries_opponent_difficulty_check;
+alter table public.sport_entries add constraint sport_entries_opponent_difficulty_check
+check (opponent_difficulty is null or (event_type = 'Zápas' and opponent_difficulty between 1 and 5));
+commit;
